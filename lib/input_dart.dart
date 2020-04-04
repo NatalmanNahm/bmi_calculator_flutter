5e2e3abed98ce841021f749c34c14719c6334734
Natalman <natalnahm@gmail.com>
2020-04-04 13:59:45 -0500

    Added a new contants class to refer to all constants. And changed all name to start with k.

--- lib/input_dart.dart
+++ lib/input_dart.dart
@@ -3,11 +3,7 @@ import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 import 'icon_content.dart';
 import 'reusable_card.dart';
-
-const bottomContainerHeight = 80.0;
-const activeCardColor = Color(0xFF1D1E33);
-const inactiveCardColor = Color(0xFF111328);
-const bottomContainerColor = Color(0xFFEB1555);
+import 'constants.dart';
 enum Gender {male, female}
 
 class InputPage extends StatefulWidget {
@@ -37,8 +33,8 @@ class _InputPageState extends State<InputPage> {
                             selectedGender = Gender.male;
                           });
                         },
-                        colour: selectedGender == Gender.male ? activeCardColor
-                            : inactiveCardColor,
+                        colour: selectedGender == Gender.male ? kActiveCardColor
+                            : kInactiveCardColor,
                         cardChild: IconContent(
                           awesomeIcons: FontAwesomeIcons.mars,
                           iconText: 'MALE',
@@ -52,8 +48,8 @@ class _InputPageState extends State<InputPage> {
                             selectedGender = Gender.female;
                           });
                         },
-                        colour: selectedGender == Gender.female ? activeCardColor
-                            : inactiveCardColor,
+                        colour: selectedGender == Gender.female ? kActiveCardColor
+                            : kInactiveCardColor,
                         cardChild: IconContent(
                             awesomeIcons: FontAwesomeIcons.venus,
                             iconText: 'FEMALE'
@@ -63,20 +59,29 @@ class _InputPageState extends State<InputPage> {
                 ],
               )
           ),
-          Expanded(child: ReusableCard(colour: activeCardColor)),
+          Expanded(
+              child: ReusableCard(
+                colour: kActiveCardColor,
+                cardChild: Column(
+                  children: <Widget>[
+                    
+                  ],
+                ),
+              ),
+          ),
           Expanded(
               child: Row(
                 children: <Widget>[
-                  Expanded(child: ReusableCard(colour: activeCardColor)),
-                  Expanded(child: ReusableCard(colour: activeCardColor)),
+                  Expanded(child: ReusableCard(colour: kActiveCardColor)),
+                  Expanded(child: ReusableCard(colour: kActiveCardColor)),
                 ],
               )
           ),
           Container(
-            color: bottomContainerColor,
+            color: kBottomContainerColor,
             margin: EdgeInsets.only(top: 10.00),
             width: double.infinity,
-            height: bottomContainerHeight,
+            height: kBottomContainerHeight,
           ),
         ],
       )
